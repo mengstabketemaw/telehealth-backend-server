38b7145eb88abf08704d723be30e80926f169375
mengstab <mengstab@gmail.com>
2022-05-13 00:04:14 -0700

    some endpoint has been added but test are not performed.

--- src/main/java/io/telehelth/authorizationserver/controller/FileController.java
+++ src/main/java/io/telehelth/authorizationserver/controller/FileController.java
@@ -2,18 +2,22 @@ package io.telehelth.authorizationserver.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.telehelth.authorizationserver.entity.Doctor;
-import io.telehelth.authorizationserver.entity.User;
+import io.telehelth.authorizationserver.entity.*;
+import io.telehelth.authorizationserver.model.UserModel;
 import io.telehelth.authorizationserver.repository.DoctorRepository;
 import io.telehelth.authorizationserver.repository.PatientRepository;
 import io.telehelth.authorizationserver.repository.UserRepository;
+import org.apache.coyote.Response;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.print.Doc;
+import javax.validation.Valid;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
@@ -49,17 +53,53 @@ public class FileController {
                 .header(HttpHeaders.CONTENT_DISPOSITION,"inline; filename=\""+doctor.getSpecializationDocument().getName()+"\"")
                 .body(doctor.getSpecializationDocument().getData());
     }
+
     @GetMapping("/id/{id}")
-    public ResponseEntity<Map<String,Object>> getUserInformationById(@PathVariable long id){
+    public ResponseEntity<Object> getUserInformationById(@PathVariable long id){
         //returning the user information vary according to the user so, first we should know what kind of user this is.
         User user = userRepository.findById(id).get();
-        Map<String,Object> response = new HashMap<>();
-        response.put("name",user.getFirstname()+" "+user.getMiddlename()+" "+user.getLastname());
-        response.put("sex",user.getSex());
-        response.put("birthDate",user.getBirthDate());
-
-        return null;
+        if(user.getRole().equals(Roles.PATIENT)){
+            Patient patient = patientRepository.findByUserId(user.getId()).get();
+            return ResponseEntity.ok(patient);
+        }else if(user.getRole().equals(Roles.DOCTOR)){
+            Doctor doctor = doctorREpository.findByUser(user).get();
+            return ResponseEntity.ok(doctor);
+        }
+            return ResponseEntity.ok(user);
     }
 
+    @GetMapping("/username/{email}")
+    public ResponseEntity<Object> getUserInformationByEmail(@PathVariable String email){
+        //returning the user information vary according to the user so, first we should know what kind of user this is.
+        User user = userRepository.findUserByEmail(email).get();
+        if(user.getRole().equals(Roles.PATIENT)){
+            Patient patient = patientRepository.findByUserId(user.getId()).get();
+            return ResponseEntity.ok(patient);
+        }else if(user.getRole().equals(Roles.DOCTOR)){
+            Doctor doctor = doctorREpository.findByUser(user).get();
+            return ResponseEntity.ok(doctor);
+        }
+        return ResponseEntity.ok(user);
+    }
 
+    @PutMapping("/")
+    public ResponseEntity<User> updateUserInformation(@ModelAttribute @Valid UserModel user) throws IOException {
+        User oldUser = userRepository.findUserByEmail(user.getEmail()).get();
+        BeanUtils.copyProperties(user,oldUser,"id","avatar","role","password");
+        return ResponseEntity.ok(userRepository.save(oldUser));
+    }
+
+    @PutMapping("/avatar")
+    public ResponseEntity<Map<String,String>> updateAvatar(@RequestPart MultipartFile file, Authentication authentication){
+        Map<String,String> response = new HashMap<>();
+        response.put("name",authentication.getName());
+        return ResponseEntity.ok(response);
+    }
 }
+/*
+file
+1.doctor file returning based on the username or user id
+2.returning avatar of the user based on the username or the id
+3.updating user information.
+user info
+*/
--- src/main/java/io/telehelth/authorizationserver/repository/DoctorRepository.java
+++ src/main/java/io/telehelth/authorizationserver/repository/DoctorRepository.java
@@ -1,9 +1,11 @@
 package io.telehelth.authorizationserver.repository;
 
 import io.telehelth.authorizationserver.entity.Doctor;
+import io.telehelth.authorizationserver.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface DoctorRepository extends JpaRepository<Doctor,Long> {
+    Optional<Doctor> findByUser(User id);
 }
