67fc97cdaceb612689148f8b0239c9c13e56bcba
Mengstab Ketemaw <mengstabrealk28@gmail.com>
2022-05-24 06:46:09 -0700

    change the error code name to reflect the organization.

--- src/main/java/io/telehelth/authorizationserver/exception/ErrorCode.java
+++ src/main/java/io/telehelth/authorizationserver/exception/ErrorCode.java
@@ -9,21 +9,21 @@ package io.telehelth.authorizationserver.exception;
  **/
 public enum ErrorCode {
   // Internal Errors: 1 to 0999
-  GENERIC_ERROR("PACKT-0001", "The system is unable to complete the request. Contact system support."),
-  HTTP_MEDIATYPE_NOT_SUPPORTED("PACKT-0002", "Requested media type is not supported. Please use application/json or application/xml as 'Content-Type' header value"),
-  HTTP_MESSAGE_NOT_WRITABLE("PACKT-0003", "Missing 'Accept' header. Please add 'Accept' header."),
-  HTTP_MEDIA_TYPE_NOT_ACCEPTABLE("PACKT-0004", "Requested 'Accept' header value is not supported. Please use application/json or application/xml as 'Accept' value"),
-  JSON_PARSE_ERROR("PACKT-0005", "Make sure request payload should be a valid JSON object."),
-  HTTP_MESSAGE_NOT_READABLE("PACKT-0006", "Make sure request payload should be a valid JSON or XML object according to 'Content-Type'."),
-  HTTP_REQUEST_METHOD_NOT_SUPPORTED("PACKT-0007", "Request method not supported."),
-  CONSTRAINT_VIOLATION("PACKT-0008", "Validation failed."),
-  ILLEGAL_ARGUMENT_EXCEPTION("PACKT-0009", "Invalid data passed."),
-  RESOURCE_NOT_FOUND("PACKT-0010", "Requested resource not found."),
-  CUSTOMER_NOT_FOUND("PACKT-0011", "Requested customer not found."),
-  ITEM_NOT_FOUND("PACKT-0012", "Requested item not found."),
-  GENERIC_ALREADY_EXISTS("PACKT-0013", "Already exists."),
-  ACCESS_DENIED("PACKT-0014", "Access Denied."),
-  UNAUTHORIZED("PACKT-0015", "Unauthorized");
+  GENERIC_ERROR("TeleHealth-0001", "The system is unable to complete the request. Contact system support."),
+  HTTP_MEDIATYPE_NOT_SUPPORTED("TeleHealth-0002", "Requested media type is not supported. Please use application/json or application/xml as 'Content-Type' header value"),
+  HTTP_MESSAGE_NOT_WRITABLE("TeleHealth-0003", "Missing 'Accept' header. Please add 'Accept' header."),
+  HTTP_MEDIA_TYPE_NOT_ACCEPTABLE("TeleHealth-0004", "Requested 'Accept' header value is not supported. Please use application/json or application/xml as 'Accept' value"),
+  JSON_PARSE_ERROR("TeleHealth-0005", "Make sure request payload should be a valid JSON object."),
+  HTTP_MESSAGE_NOT_READABLE("TeleHealth-0006", "Make sure request payload should be a valid JSON or XML object according to 'Content-Type'."),
+  HTTP_REQUEST_METHOD_NOT_SUPPORTED("TeleHealth-0007", "Request method not supported."),
+  CONSTRAINT_VIOLATION("TeleHealth-0008", "Validation failed."),
+  ILLEGAL_ARGUMENT_EXCEPTION("TeleHealth-0009", "Invalid data passed."),
+  RESOURCE_NOT_FOUND("TeleHealth-0010", "Requested resource not found."),
+  CUSTOMER_NOT_FOUND("TeleHealth-0011", "Requested customer not found."),
+  ITEM_NOT_FOUND("TeleHealth-0012", "Requested item not found."),
+  GENERIC_ALREADY_EXISTS("TeleHealth-0013", "Already exists."),
+  ACCESS_DENIED("TeleHealth-0014", "Access Denied."),
+  UNAUTHORIZED("TeleHealth-0015", "Unauthorized");
 
   private String errCode;
   private String errMsgKey;
